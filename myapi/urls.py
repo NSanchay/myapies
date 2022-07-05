5be924dcec52ae81946b334e29eb56d5ba938769
myusername <nandanikupa@gmail.com>
2022-07-05 10:10:32 -0700

    update

--- myapi/urls.py
+++ myapi/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
